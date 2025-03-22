9e2745ebf607df68af8581fdb12c630945f05d6c
mackswell13 <maxsherwin1323@gmail.com>
2025-03-22 17:16:17 -0500

    Update reauthentication route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
+  allow_unauthenticated_access only: [ :reauth ]
+
   def home
     puts "Cookies: ", cookies.to_hash
     puts "Auth?: ", authenticated?
     @store = Current.session.store
     @user = Current.user
     @session = Current.session
+  end
+
+  def reauth
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
+  # resource :session
+  # resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "/load", to: "auth#load"
 
   get "/home", to: "pages#home"
+  get "/reauth", to: "pages#reauth"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
